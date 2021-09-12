bcad73329a4b6e0ebaac981f058032a188b5b2f2
Eladio Junior Rodriguez Rodriguez <eladiojuniorrodriguez@gmail.com>
2021-09-12 08:15:03 -0400

    Added Models

---
+++ MiDominicanaApi/Models/Currency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiDominicanaApi.Models
+{
+    public class Currency
+    {
+        public string Bank { get; set; }
+        public byte Rate { get; set; }
+        public string Date { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ MiDominicanaApi/Models/Fuel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiDominicanaApi.Models
+{
+    public class Fuel
+    {
+        public string Name { get; set; }
+        public ushort Price { get; set; }
+        public string Date { get; set; }
+    }
+}
